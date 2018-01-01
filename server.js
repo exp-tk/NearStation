af67fc652da6c0b1acfe02b0da057c7447fa7b70
TinyKitten <catwalk8205@gmail.com>
2018-01-01 11:26:55 +0900

    https強制

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-const path = require('path');
-app.get('/*', function(req, res){
+
+function forceHttps(req, res, next){
+  if (!process.env.PORT) {
+    return next();
+  };
+
+  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+    res.redirect('https://' + req.headers.host + req.url);
+  } else {
+    return next();
+  }
+};
+
+app.all('*', forceHttps);
+
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-console.log('Server listening on port 8080');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
