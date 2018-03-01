72766b00f83348e4498ed628e7fa9b5417d58afd
TinyKitten <catwalk8205@gmail.com>
2018-03-02 01:37:59 +0900

    #7

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config = {
-  WSEndpoint: 'ws://localhost:3000/ws',
+  WSEndpoint: 'wss://sapi.tinykitten.me/ws',
+  DicEndpoint: 'https://sapi.tinykitten.me/v1/dic.json',
 };
 export default config;
