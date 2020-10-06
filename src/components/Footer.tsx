f34ffc19a8a6ec69bd38bb03073dd99740fdeb1f
TinyKitten <ts@tinykitten.me>
2020-10-06 22:02:53 +0900

    フッター調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,26 @@
 import React, { memo } from 'react';
 import styles from './Footer.module.css';
-import tinykitten from '../assets/tinykitten.svg';
 
 const Footer: React.FC = () => (
   <footer className={styles.footer}>
-    <a
-      href="https://tinykitten.me"
-      rel="noopener noreferrer"
-      target="_blank"
-      className={[styles.link, styles.productof].join(' ')}
-    >
-      A product of&nbsp;
-      <img src={tinykitten} alt="TinyKitten" />
-    </a>
-    <p className={[styles.link, styles.copyright].join(' ')}>
-      Copyright &copy; {new Date().getFullYear()}&nbsp;
-      <a rel="noopener noreferrer" target="_blank" href="https://tinykitten.me">
-        TinyKitten
+    <p className={styles.marginBottom}>
+      <a
+        href="https://github.com/TinyKitten/NearStation"
+        rel="noopener noreferrer"
+        target="_blank"
+        className={styles.link}
+      >
+        Fork me on GitHub
+      </a>
+    </p>
+    <p>
+      <a
+        className={styles.link}
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://tinykitten.me"
+      >
+        Copyright &copy;&nbsp;{new Date().getFullYear()}&nbsp;TinyKitten
       </a>
     </p>
   </footer>
